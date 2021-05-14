e00c3a12e0d1748b3a82e785bf52e84d85426640
kkusche <katharina.kusche@gmail.com>
2021-05-14 16:07:01 +0200

    fix styling issues at forecast tiles

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,47 +2,36 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
-
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
-
   useEffect(() => {
     setLoaded(false);
   }, [props.coordinates]);
-
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
-
   function load() {
     let apiKey = "c66576c07870c288050a291536fab892";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
     axios.get(apiUrl).then(handleResponse);
   }
-
   if (loaded) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-            {forecast.map(function (dailyForecast, index) {
-                if (index < 5) {
-                    return (
-                        <div key={index}>
-                            <WeatherForecastDay data={dailyForecast} />
-                        </div>
-                    );
-                } else {
-                    return null;
-                    }
-                })}
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return <WeatherForecastDay key={index} data={dailyForecast} />;
+            } else {
+              return null;
+            }
+          })}
         </div>
-        </div>
-
+      </div>
     );
   } else {
     load();
